20d66b2ebcfcac69ee12911bcf1cd8754adc1c9e
Ciaran Archer <carcher@zendesk.com>
2013-05-31 16:42:36 +0100

    Displaying movies data.

--- app/js/models/list.js
+++ app/js/models/list.js
@@ -1,4 +1,17 @@
 Movies.List = DS.Model.extend({
-  name: DS.attr('string'),
+  title: DS.attr('string'),
   movies: DS.hasMany('Movies.Movie')
 });
+
+// ... additional lines truncated for brevity ...
+Movies.List.FIXTURES = [{
+    id: 1,
+    title: 'Robert De Niro Movies'
+  }, {
+    id: 2,
+    title: 'James Cameron Movies'
+  }, {
+    id: 3,
+    title: 'Schwarzenegger Classics'
+  }
+];
--- app/js/router.js
+++ app/js/router.js
@@ -1,3 +1,9 @@
 Movies.Router.map(function () {
   this.resource('movies', { path: '/' });
 });
+
+Movies.MoviesRoute = Ember.Route.extend({
+  model: function () {
+    return Movies.List.find();
+  }
+});
