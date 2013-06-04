132829214c17735593f55d5b5e92b2c9333775f9
Ciaran Archer <carcher@zendesk.com>
2013-06-04 21:56:53 +0100

    Removed fixtures.

--- app/js/models/list.js
+++ app/js/models/list.js
@@ -2,16 +2,3 @@ Movies.List = DS.Model.extend({
   title: DS.attr('string'),
   movies: DS.hasMany('Movies.Movie')
 });
-
-Movies.List.FIXTURES = [{
-    id: 1,
-    title: 'Robert De Niro Movies',
-    movie_ids: [1, 2]
-  }, {
-    id: 2,
-    title: 'James Cameron Movies'
-  }, {
-    id: 3,
-    title: 'Schwarzenegger Classics'
-  }
-];
