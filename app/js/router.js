bd6861a2410d35c142ef4db62cfe651aa58ae998
Ciaran Archer <carcher@zendesk.com>
2013-06-08 19:19:45 +0100

    Added missing semicolon.

--- app/js/router.js
+++ app/js/router.js
@@ -1,6 +1,6 @@
 Movies.Router.map(function () {
   this.resource('lists', function() {
-    this.resource('list', { path: '/:list_id' })
+    this.resource('list', { path: '/:list_id' });
   });
 
   this.route('boxoffice');
