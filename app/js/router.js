5f07f0d5ad30d98e3b38000d12048b0044534fa3
Ciaran Archer <carcher@zendesk.com>
2013-06-08 20:59:54 +0100

    Removed nested routing across the app. Too troublesome.

--- app/js/router.js
+++ app/js/router.js
@@ -1,10 +1,8 @@
 Movies.Router.map(function () {
-  this.resource('list', { path: '/list' }, function() {
-    this.resource('movies', { path: '/:list_id/movies' }, function() {
-    });
-  });
-  this.resource('notes', { path: 'movies/:movie_id/notes' }, function() {
-  });
+  this.resource('list', { path: '/list' });
+  // TODO: Resource routes with placeholders seems to require the empty function
+  this.resource('movies', { path: 'list/:list_id/movies' }, function() {});
+  this.resource('notes', { path: 'movies/:movie_id/notes' }, function() {});
   this.route('boxoffice');
 });
 
--- app/js/routes/movies_route.js
+++ app/js/routes/movies_route.js
@@ -1,11 +1,5 @@
 Movies.MoviesRoute = Ember.Route.extend({
   model: function(params) {
     return Movies.List.find(params.list_id);
-  },
-
-  renderTemplate: function() {
-    this.render('movies', {   // the template to render
-      into: 'application' // template outlet to render into (will mess up your back button)
-    });
   }
 });
